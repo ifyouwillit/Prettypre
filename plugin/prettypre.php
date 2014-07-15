f875a66cae9327d10a8134562e7c5bb8ed622b26
ifyouwillit <git@mendel.me>
2014-07-14 22:26:35 -0500

    Updated path to plugins

--- plugin/prettypre.php
+++ plugin/prettypre.php
@@ -27,7 +27,7 @@
 require_once( plugin_dir_path( __FILE__ ) . '/options.php' );
 
 function change_to_pretty_pre() {
-	wp_enqueue_style( "prettypre", "/wp-content/plugins/prettypre/prettyprecss.php?style=".get_option('selectstyle')); 
+	wp_enqueue_style( "prettypre", plugins_url( "/prettyprecss.php?style=".get_option('selectstyle') , __FILE__ )); 
 }
 
 add_action ( 'wp_enqueue_scripts', 'change_to_pretty_pre' );
