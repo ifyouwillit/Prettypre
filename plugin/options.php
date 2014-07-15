801569f8ec89ea8dcc50fea5cae53488fddcf53d
ifyouwillit <git@mendel.me>
2014-07-15 17:29:36 -0500

    removed jquery for time being

--- plugin/options.php
+++ plugin/options.php
@@ -5,20 +5,18 @@ add_action('admin_menu', 'prettypre_create_menu');
 function prettypre_create_menu() {
 
 	//create new top-level menu
-	add_menu_page('Pretty Pre', 'Pretty Pre', 'administrator',__FILE__, 'prettypre_settings_page');
+	$page_hook_suffix = add_menu_page('Pretty Pre', 'Pretty Pre', 'administrator',__FILE__, 'prettypre_settings_page');
 
 	//call register settings function
 	add_action( 'admin_init', 'register_mysettings' );
-	
 	add_action('admin_print_scripts-' . $page_hook_suffix, 'prettypre_admin_scripts');
 }
 
 function prettypre_admin_scripts() {
 
 	/* Link our already registered script to a page */
-	wp_enqueue_script( 'jquery-ui-core' );
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_style( 'prettypre-admin',plugins_url( "/prettyprecss.php?admin=1&style=".get_option('selectstyle')."&ts=".get_option('textsize'), __FILE__ ));
+
+	wp_enqueue_style( 'prettypre-admin',plugins_url("/prettyprecss.php?admin=1&style=".get_option('selectstyle')."&ts=".get_option('textsize'), __FILE__ ));
 }
 
 function register_mysettings() {
@@ -37,25 +35,6 @@ function prettypre_settings_page() {
 
 	<h2>Pretty Pre Settings</h2>
 	
-	<script>
-
-		$(function() {
-
-		$( "#slider-range-max" ).slider({
-			range: "max",
-			min: 6,
-			max: 24,
-			value: <? if (get_option('textsize')>"0") { echo get_option('textsize'); } else { echo "10"; }?>,
-			slide: function( event, ui ) {
-				$( "#textsize" ).val( ui.value );
-			}
-		});
-
-		$( "#textsize" ).val( $( "#slider-range-max" ).slider( "textsize" ) );
-		});
-
-	</script>
-
 	<form method="post" action="options.php">
 
 		<?php settings_fields( 'prettypre-settings-group' ); ?>
@@ -81,8 +60,20 @@ function prettypre_settings_page() {
 		<div style="width:50%;">
 		
 			<label for="amount">Font size:</label>
-			<input type="text" id="textsize" name="textsize"<? if (get_option('textsize')>"0") { echo "value=\"".get_option('textsize')."\""; }?>readonly style="border:0; color:#f6931f; font-weight:bold;">
-
+			<select name="textsize">
+				<option value="<? if (get_option('textsize')>"0") { echo get_option('textsize'); } else { echo "10"; }?>"><? if (get_option('textsize')>"0") { echo get_option('textsize'); } else { echo "10"; }?>px</option>
+				<option value="6">6px</option>
+				<option value="8">8px</option>
+				<option value="10">10px</option>
+				<option value="12">12px</option>
+				<option value="14">14px</option>
+				<option value="16">16px</option>
+				<option value="18">18px</option>
+				<option value="20">20px</option>
+				<option value="22">22px</option>
+				<option value="24">24px</option>
+			</select>
+			
 		</div>
 <div id="slider-range-max"></div>
 
@@ -98,3 +89,4 @@ function prettypre_settings_page() {
 
 This is an open source project and a work in progress.  You can learn more at <a href="http://prettypre.com" target="_blank">The Prettypre Website</a> or contribute to the plugin at <a href="https://github.com/ifyouwillit/Prettypre" target="_blank">Github</a>.
 <?php } ?>
+
